ce97092c94e28fbd4c7a09b54a3cbac8fa61a211
Philip Bosson <81214535+interwhy@users.noreply.github.com>
2025-08-26 21:52:14 -0700

    Use built-in switch-source-header command

--- private_dot_config/nvim/lua/xanadu/remap.lua
+++ private_dot_config/nvim/lua/xanadu/remap.lua
@@ -2,28 +2,6 @@ local which_key = require "which-key"
 local builtin = require "telescope.builtin"
 local util = require "lspconfig.util"
 
--- copied from: https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/configs/clangd.lua
--- https://clangd.llvm.org/extensions.html#switch-between-sourceheader
-local function switch_source_header(bufnr)
-  local method_name = 'textDocument/switchSourceHeader'
-  bufnr = util.validate_bufnr(bufnr)
-  local client = vim.lsp.get_clients({ bufnr = bufnr, name = 'clangd' })[1]
-  if not client then
-    return vim.notify(('method %s is not supported by any servers active on the current buffer'):format(method_name))
-  end
-  local params = vim.lsp.util.make_text_document_params(bufnr)
-  client:request(method_name, params, function(err, result)
-    if err then
-      error(tostring(err))
-    end
-    if not result then
-      vim.notify('corresponding file cannot be determined')
-      return
-    end
-    vim.cmd.edit(vim.uri_to_fname(result))
-  end, bufnr)
-end
-
 local lsp_mappings = {
   { "gd", vim.lsp.buf.definition, desc = "Go to definition" },
   { "<leader>l", group = "LSP" },
@@ -36,10 +14,20 @@ local lsp_mappings = {
   { "<leader>lw", builtin.workspace_symbols, desc = "Workspace symbol" },
   { "<leader>ld", builtin.diagnostics, desc = "Diagnostics" },
   { "<leader>ll", vim.diagnostic.open_float, desc = "Open diagnostic float" },
-  { "<leader>lh", function() switch_source_header(0); end, desc = "Switch source header" },
+  { "<leader>lh", ":LspClangdSwitchSourceHeader<CR>", desc = "Switch source header" },
 }
 
 which_key.add(lsp_mappings)
+
+-- TODO: why doesn't this work?
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "*.c", "*.cc", "*.cpp", "*.cxx", "*.h", "*.hpp", "*.hxx", "*.cppm", "*.ixx" },
+  callback = function(args)
+    which_key.add {
+      { "<leader>lf", ":ClangFormat<CR>", desc = "Run clang-format" },
+    }
+  end
+})
 
 local debug_mappings = {
   { "<F5>", function() require('dap').continue() end, desc = "Continue", mode = "n" },
